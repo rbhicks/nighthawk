5734510ae0b5d4a9598d4215ff843083aae0daec
rbhicks <r.b.hicks@gmail.com>
2020-06-01 17:23:19 +0200

    rule compile working.

--- lib/nighthawk/nighthawk_server.ex
+++ lib/nighthawk/nighthawk_server.ex
@@ -6,33 +6,23 @@ defmodule Nighthawk.NighthawkServerMacros do
     end
   end
 
-  defmacro defrule({rule_name, _, _}, do: block) do
-    # IO.puts "§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§"
-    # IO.puts "§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§"
-    # IO.inspect rule_name
-    # IO.puts "§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§"
-    # IO.inspect block
-    # IO.puts "§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§"
-    # IO.puts "§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§"
+  defmacro defrule({_rule_name, _, _}, do: {:__block__,
+                                           _,
+                                           [{:conditions, _, _},
+                                            {:actions, _, _}] = conditions_and_actions}) do
     quote do
-      unquote block
-    end
-  end
-
-  defmacro conditions(do: block) do
-    quote do
-      unquote do
-        block |> process_conditions |> IO.inspect
+      if (unquote (conditions_and_actions |> hd)) do
+        unquote (conditions_and_actions |> tl)
       end
     end
   end
 
+  defmacro conditions(do: block) do
+    block |> process_conditions
+  end
+
   defmacro actions(do: block) do
-    # IO.puts "*********************************"
-    # IO.puts "*********************************"
-    # IO.inspect block
-    # IO.puts "*********************************"
-    # IO.puts "*********************************"
+    block
   end
 
   # the first tuple element is  :__block__
@@ -43,21 +33,8 @@ defmodule Nighthawk.NighthawkServerMacros do
 
   # the first tuple element wasn't :__block__
   # we know we have only one condition
-  def process_conditions({function, _meta, arguments}) do
+  def process_conditions({function, _meta, _arguments}) do
     {function, [context: Elixir, import: Kernel], [true, true]}    
-  end
-
-  # this catches the last recursive call
-  def process_conditions([], condition_logic), do: condition_logic
-
-  # primary recursive processor
-  def process_conditions([head|tail], conditions_ast) do
-    process_conditions(tail,
-      {:&&, [context: Elixir, import: Kernel],
-       [
-         conditions_ast,
-         head
-       ]})
   end
 
   # pattern match to handle the case where there are only
@@ -79,6 +56,38 @@ defmodule Nighthawk.NighthawkServerMacros do
                        create_condition_ast_core(head, tail |> hd))
   end
 
+  # this catches the last recursive call
+  def process_conditions([], condition_logic), do: condition_logic
+
+  # primary recursive processor
+  def process_conditions([head|tail], conditions_ast) do
+    process_conditions(tail,
+      {:&&, [context: Elixir, import: Kernel],
+       [
+         conditions_ast,
+         head
+       ]})
+  end
+
+  # # pattern match to handle the case where there are only
+  # # two conditions. this way we can use pattern matching
+  # # instead of internal conditional logic
+  # def process_conditions([first_condition, second_condition]) do
+  #   create_condition_ast_core(first_condition, second_condition)
+  # end
+
+  # # pattern match to handle the first two conditions where
+  # # there are more than two conditions. the first two need
+  # # to be handled separately since they form the core of
+  # # the condition ast. we're using pattern matching to do
+  # # this to avoid having a single function with conditional
+  # # logic to handle the special case of only two conditions
+  # # along with the cases of more than two conditions
+  # def process_conditions([head|tail]) do
+  #   process_conditions(tail |> tl,
+  #                      create_condition_ast_core(head, tail |> hd))
+  # end
+
   def create_condition_ast_core(first_condition, second_condition) do
     {:&&,
      [context: Elixir, import: Kernel],
@@ -88,7 +97,6 @@ defmodule Nighthawk.NighthawkServerMacros do
      ]}
   end
 end
-
 
 defmodule Nighthawk.NighthawkServer do
   use GenServer
@@ -103,56 +111,33 @@ defmodule Nighthawk.NighthawkServer do
   end
 
   def init(_args) do
-
-
-    # - backlinks
-    # -- backlink_total
-    # -- bridgelink_candidate_total
-    # - internal_links
-    # -- internal_link_total
-    # source: https//:someplace.com/some-path/some-page.html?some-query-string
-
-    defrule bad_source_for_backlink do
-      conditions do
-        true == true
-        13 < 17
-        17 > 11
-        false == false
-      end
-      actions do
-        IO.puts "$%$%$%$%$%$%$%$%$%$%$%$%"
-        IO.puts "@@@@@@@@@@@@@@@@@@@@@@@@"
-      end
-    end
-
-    # {:if, [context: Elixir, import: Kernel],
-    #  [
-    #    true,
-    #    [
-    #      do: {{:., [], [{:__aliases__, [alias: false], [:IO]}, :puts]}, [],
-    #           ["$$$$$$$$$$"]}
-    #    ]
-    #  ]}
-        
-    # {:&&, [context: Elixir, import: Kernel],
-    #  [
-    #    {:&&, [context: Elixir, import: Kernel],
-    #     [
-    #       {:&&, [context: Elixir, import: Kernel],
-    #        [
-    #          {:==, [context: Elixir, import: Kernel], [true, true]},
-    #          {:<, [context: Elixir, import: Kernel], [13, 17]}
-    #        ]},
-    #       {:>, [context: Elixir, import: Kernel], [17, 11]}
-    #     ]},
-    #    {:==, [context: Elixir, import: Kernel], [false, false]}
-    #  ]}
-    
-
-    IO.puts "±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±"
     {:ok, {}}
   end
 
-
-
+  ##########################################################################################
+  ##########################################################################################
+  ##########################################################################################
+  #
+  # - backlinks
+  # -- backlink_total
+  # -- bridgelink_candidate_total
+  # - internal_links
+  # -- internal_link_total
+  # source: https//:someplace.com/some-path/some-page.html?some-query-string
+  ##########################################################################################
+  ##########################################################################################
+  ##########################################################################################
+  
+  defrule bad_source_for_backlink do
+    conditions do
+      true == true
+      13 < 17
+      17 > 11
+      false == false
+    end
+    actions do
+      IO.puts "$%$%$%$%$%$%$%$%$%$%$%$%"
+      IO.puts "@@@@@@@@@@@@@@@@@@@@@@@@"
+    end
+  end
 end
